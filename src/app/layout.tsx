bb69252f7f2fe118d0a229cc223b7619490bf0b8
Emre <kozanfurkanemre@gmail.com>
2023-02-07 21:26:34 +0300

    start pack

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import "./reset.css"
 import './globals.css'
 
 export default function RootLayout({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import "./reset.css"
-import "./globals.css"
 import { Inter } from '@next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
